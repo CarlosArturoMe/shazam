6a2fb1fe1502dc7902916fbac902f91b49389f73
Carlos Medina <spark_343@hotmail.com>
2021-04-07 13:06:29 -0500

    Se aplica ruido solo al fragmento a reproducir

--- songs_queries.sql
+++ songs_queries.sql
@@ -1,8 +1,9 @@
 SELECT * FROM music_recognition.songs;
 SELECT COUNT(*) FROM music_recognition.songs;
-SELECT * FROM music_recognition.songs where song_name like '%nandoo1%';
 
-DELETE FROM music_recognition.songs where song_id = 8004;
+SELECT * FROM music_recognition.songs where song_name like '%000756%';
+
+DELETE FROM music_recognition.songs where song_id = 348;
 DELETE FROM music_recognition.songs where song_id = 3523;
 
 describe music_recognition.fingerprints;
